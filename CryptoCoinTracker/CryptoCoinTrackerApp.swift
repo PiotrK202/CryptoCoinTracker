04f5446f3bb515827a18350326540cea119333df
PiotrK <piotr.koscielny69@gmail.com>
2025-04-03 13:45:28 +0200

    Initial Commit

---
+++ CryptoCoinTracker/CryptoCoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinTrackerApp.swift
+//  CryptoCoinTracker
+//
+//  Created by piotr koscielny on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoCoinTrackerTests/CryptoCoinTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinTrackerTests.swift
+//  CryptoCoinTrackerTests
+//
+//  Created by piotr koscielny on 3/4/25.
+//
+
+import Testing
+@testable import CryptoCoinTracker
+
+struct CryptoCoinTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
