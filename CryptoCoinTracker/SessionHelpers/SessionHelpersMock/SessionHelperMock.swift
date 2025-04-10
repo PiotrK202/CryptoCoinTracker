b57e4b680d16484a66e5d820b04b90c6ca32887a
PiotrK <piotr.koscielny69@gmail.com>
2025-04-10 15:59:08 +0200

    changed folders for Mocks

---
+++ CryptoCoinTracker/SessionHelpers/SessionHelpersMock/SessionHelperMock.swift
@@ -0,0 +1,12 @@
+//
+//  SessionHelperMock.swift
+//  CryptoCoinTracker
+//
+//  Created by piotr koscielny on 10/4/25.
+//
+
+import Foundation
+
+struct SessionHelperMock: SessionHelperProtocole {
+    var isLoggedIn: Bool
+}
